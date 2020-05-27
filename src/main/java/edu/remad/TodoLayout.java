f44fe3655c2e8ab75c9508c4ccd9f66613aa4e4e
RenatiusvanSander / R. Meier <remad@web.de>
2020-05-27 22:32:23 +0200

    webapp: Implemented first UI and expanded components

---
+++ src/main/java/edu/remad/TodoLayout.java
@@ -0,0 +1,10 @@
+package edu.remad;
+
+import com.vaadin.spring.annotation.SpringComponent;
+import com.vaadin.ui.VerticalLayout;
+
+@SpringComponent
+public class TodoLayout extends VerticalLayout {
+
+
+}
---
+++ src/main/java/edu/remad/Vaadin8SampleApplication.java
@@ -0,0 +1,12 @@
+package edu.remad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vaadin8SampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Vaadin8SampleApplication.class, args);
+    }
+}
